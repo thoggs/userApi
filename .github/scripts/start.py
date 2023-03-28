987f369c5b5c8498f17ecbb7af550d684b479932
Thiago Rodrigues <thiagothogs@icloud.com>
2023-03-28 01:45:54 -0300

    update start.py

--- .github/scripts/start.py
+++ .github/scripts/start.py
@@ -1,49 +1,40 @@
-#!/usr/bin/env python3
-import subprocess
-import sys
-from pathlib import Path
-
+import os
 from tqdm import tqdm
 
 
-def run_command(cmd, desc):
-    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
-        for line in tqdm(iter(proc.stdout.readline, b''), desc=desc, unit='B', unit_scale=True):
-            sys.stdout.write(line.decode('utf-8'))
-            sys.stdout.flush()
-        for line in tqdm(iter(proc.stderr.readline, b''), desc=desc, unit='B', unit_scale=True):
-            sys.stdout.write(line.decode('utf-8'))
-            sys.stdout.flush()
-        proc.wait()
+# Define uma função auxiliar para executar um comando com barras de progresso
+def run_with_progress(command, desc):
+    with tqdm(desc=desc, unit="") as progress:
+        for line in os.popen(command):
+            progress.update()
 
 
 # Cria o ambiente virtual na pasta .venv
-run_command(['python', '-m', 'venv', '/app/.venv'], 'Criando ambiente virtual')
+run_with_progress("python -m venv /app/.venv", "Criando ambiente virtual")
 
 # Ativa o ambiente virtual
-run_command(['bash', '-c', 'source /app/.venv/bin/activate'], 'Ativando ambiente virtual')
+activate = "/app/.venv/bin/activate"
+if os.path.isfile(activate):
+    exec(open(activate).read(), {"__file__": activate})
 
 # Define o caminho do sistema para o ambiente virtual
-run_command(['export', 'PATH="/app/.venv/bin:$PATH"'], 'Definindo caminho do sistema')
+os.environ["PATH"] = "/app/.venv/bin:" + os.environ["PATH"]
 
 # Cria a pasta static se ainda não existir
-static_dir = Path('/app/static')
-if not static_dir.is_dir():
-    static_dir.mkdir(parents=True)
-    print(f"Criando pasta {static_dir}.")
+if not os.path.isdir("/app/static"):
+    os.mkdir("/app/static")
 
 # Copia os arquivos do projeto para o diretório /app
-run_command(['cp', '-r', '.', '/app'], 'Copiando arquivos')
+run_with_progress("cp -r . /app", "Copiando arquivos")
 
 # Instala pacotes necessários
-run_command(['pip', 'install', '--upgrade', 'pip'], 'Atualizando pip')
-run_command(['pip', 'install', '-r', '/app/requirements.txt'], 'Instalando pacotes')
+run_with_progress("pip install --upgrade pip && pip install -r /app/requirements.txt", "Instalando pacotes")
 
 # Executa as migrações do banco de dados
-run_command(['python', '/app/manage.py', 'migrate'], 'Executando migrações do banco de dados')
+run_with_progress("python manage.py migrate", "Executando migrações")
 
 # Coleta os arquivos estáticos do Django
-run_command(['python', '/app/manage.py', 'collectstatic'], 'Coletando arquivos estáticos do Django')
+run_with_progress("python manage.py collectstatic", "Coletando arquivos estáticos")
 
 # Inicia o servidor web Gunicorn
-run_command(['gunicorn', 'userApi.wsgi:application', '--bind', 'web:8000'], 'Iniciando servidor web')
+os.execlp("gunicorn", "gunicorn", "userApi.wsgi:application", "--bind", "web:8000")
