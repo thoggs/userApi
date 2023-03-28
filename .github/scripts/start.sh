53d7e1882a79ad88275b6ffbfe8dcd2391712e56
Thiago Rodrigues <thiagothogs@icloud.com>
2023-03-28 00:21:26 -0300

    up-date dockerfile

--- .github/scripts/start.sh
+++ .github/scripts/start.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Cria o ambiente virtual na pasta .venv
-python -m venv /app/.venv
-
-# Ativa o ambiente virtual
-source /app/.venv/bin/activate
-
-# Define o caminho do sistema para o ambiente virtual
-export PATH="/app/.venv/bin:$PATH"
-
-# Cria a pasta static se ainda não existir
-if [ ! -d "/app/static" ]; then
-    mkdir -p /app/static
-fi
-
-# Copia os arquivos do projeto para o diretório /app
-cp -r . /app
-
-# Instala pacotes necessários
-pip install --upgrade pip && pip install -r /app/requirements.txt
+## Cria o ambiente virtual na pasta .venv
+#python -m venv /app/.venv
+#
+## Ativa o ambiente virtual
+#source /app/.venv/bin/activate
+#
+## Define o caminho do sistema para o ambiente virtual
+#export PATH="/app/.venv/bin:$PATH"
+#
+## Cria a pasta static se ainda não existir
+#if [ ! -d "/app/static" ]; then
+#    mkdir -p /app/static
+#fi
+#
+## Copia os arquivos do projeto para o diretório /app
+#cp -r . /app
+#
+## Instala pacotes necessários
+#pip install --upgrade pip && pip install -r /app/requirements.txt
 
 # Executa as migrações do banco de dados
 python manage.py migrate
